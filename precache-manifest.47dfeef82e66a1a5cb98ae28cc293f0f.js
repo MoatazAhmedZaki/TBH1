811eac10102bbd2c34cb530b86e00ea0c4ec4afb
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 16:23:10 +0200

    Updates

---
+++ precache-manifest.47dfeef82e66a1a5cb98ae28cc293f0f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f833fbc69ffe4cce190d71f91204c8",
+    "url": "/TBH1/index.html"
+  },
+  {
+    "revision": "0bcd124b1530a44d4d41",
+    "url": "/TBH1/static/css/main.cf01961e.chunk.css"
+  },
+  {
+    "revision": "514c08ca8d39f0a2a18b",
+    "url": "/TBH1/static/js/2.71af4608.chunk.js"
+  },
+  {
+    "revision": "0bcd124b1530a44d4d41",
+    "url": "/TBH1/static/js/main.4067b2b7.chunk.js"
+  },
+  {
+    "revision": "5b556826d8467be882d6",
+    "url": "/TBH1/static/js/runtime-main.2334034d.js"
+  },
+  {
+    "revision": "7345163686f0908ea27c48afca3191c0",
+    "url": "/TBH1/static/media/blogBg.73451636.png"
+  },
+  {
+    "revision": "32f2f78199a0b51cac030015531136f4",
+    "url": "/TBH1/static/media/blogtablet.32f2f781.png"
+  },
+  {
+    "revision": "3df28fba90b45da0891800fe0b520fd8",
+    "url": "/TBH1/static/media/bluredImg.3df28fba.png"
+  },
+  {
+    "revision": "f61ad28e543f810817603597ba24ba6c",
+    "url": "/TBH1/static/media/contactBg.f61ad28e.png"
+  },
+  {
+    "revision": "5d1a9f82e9e60e60edc1d920de5cde1a",
+    "url": "/TBH1/static/media/contactfooter.5d1a9f82.png"
+  },
+  {
+    "revision": "36de21a7980b92938eb2dceb71f4f810",
+    "url": "/TBH1/static/media/contacttablet.36de21a7.png"
+  },
+  {
+    "revision": "354b12564995ff4ee6321a58d1b8849d",
+    "url": "/TBH1/static/media/garage.354b1256.png"
+  },
+  {
+    "revision": "9bfe8bf511a08aecc0f6b9bd5d2dcd6f",
+    "url": "/TBH1/static/media/homebg.9bfe8bf5.png"
+  },
+  {
+    "revision": "b826e60e936d0df6ab2c558ba9d06942",
+    "url": "/TBH1/static/media/homebgtablet.b826e60e.png"
+  },
+  {
+    "revision": "fca41def322ceb43f522ee8cf383ca0c",
+    "url": "/TBH1/static/media/spacebg.fca41def.png"
+  },
+  {
+    "revision": "20b519672c5227d7f89debaf6119e58b",
+    "url": "/TBH1/static/media/spacetablet.20b51967.png"
+  }
+]);
